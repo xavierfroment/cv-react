5a5c3f57c799bba28601c3dd5687d7b2b8b3bf33
fromentxavier <fromentxavier@gmail.com>
2021-01-24 21:07:00 +0100

    test 3

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Menu from './Menu';
 import Home from './Accueil/Home';
 import Experiences from './Parcours/Experiences';
