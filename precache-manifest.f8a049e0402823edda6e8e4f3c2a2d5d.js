c194ad05fdf098b5d4172097e9d6826ee1514496
fromentxavier <fromentxavier@gmail.com>
2021-01-24 18:43:10 +0100

    Updates

---
+++ precache-manifest.f8a049e0402823edda6e8e4f3c2a2d5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700101365bf2cb9ff0a57d4643581ee9",
+    "url": "/cv-react/index.html"
+  },
+  {
+    "revision": "8bd1dfd21918e778185e",
+    "url": "/cv-react/static/css/3.de424728.chunk.css"
+  },
+  {
+    "revision": "bc47ff176a7fc092e19b",
+    "url": "/cv-react/static/css/4.f805cc15.chunk.css"
+  },
+  {
+    "revision": "fbe91fb864cf4aaf7d47",
+    "url": "/cv-react/static/css/main.f0fcd6c4.chunk.css"
+  },
+  {
+    "revision": "3c9fbf8700f06b9c5036",
+    "url": "/cv-react/static/js/2.fa6a6dbc.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "/cv-react/static/js/2.fa6a6dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd1dfd21918e778185e",
+    "url": "/cv-react/static/js/3.0a62423c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-react/static/js/3.0a62423c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc47ff176a7fc092e19b",
+    "url": "/cv-react/static/js/4.b11356da.chunk.js"
+  },
+  {
+    "revision": "fbe91fb864cf4aaf7d47",
+    "url": "/cv-react/static/js/main.83523bc2.chunk.js"
+  },
+  {
+    "revision": "bc9aa9b56e159c98b4ae",
+    "url": "/cv-react/static/js/runtime-main.69b4b86a.js"
+  },
+  {
+    "revision": "1e8fbf6121b5c25d371c12619e5f8b63",
+    "url": "/cv-react/static/media/bg.1e8fbf61.jpg"
+  }
+]);
