4db331c3c4c1c603e2d5480d43e1a228d4f5fd62
fromentxavier <fromentxavier@gmail.com>
2021-02-06 19:00:22 +0100

    ajout portfolio

--- src/components/Portfolio/dataportfolio.js
+++ src/components/Portfolio/dataportfolio.js
@@ -3,9 +3,17 @@ const dataportfolio = [
     id: 1,
     preview: "jpg/cvold.png",
     altpreview: "ancien cv",
-    lienfolio: "http://xavierfroment.github.io/cvold",
+    lienfolio: "https://xavierfroment.github.io/cvold",
     titre: "Premier Site CV (2019)",
     desc: "CV réalisé en HTML, CSS et JavaScript dans le cadre de la formation de développeur web."
+  },
+  {
+    id: 2,
+    preview: "jpg/colorshade.png",
+    altpreview: "mon nuancier",
+    lienfolio: "https://xavierfroment.github.io/color-shade/",
+    titre: "Mon Nuancier",
+    desc: "Projet réalisé en React.JS avec la bibliothèque Values.JS, pour vous aider dans vos choix de couleurs"
   }
   /*ADD SOME DATA HERE !! */ 
 
