c77eccc8de7226f8e72d0f381b2ef90a751216a4
fromentxavier <fromentxavier@gmail.com>
2021-05-03 18:48:08 +0200

    adding adoptapet and fix navbar

--- src/components/Portfolio/dataportfolio.js
+++ src/components/Portfolio/dataportfolio.js
@@ -2,7 +2,7 @@ const dataportfolio = [
   {
     id: 1,
     preview: "jpg/cvold.png",
-    altpreview: "ancien cv",
+    altpreview: "Ancien cv",
     lienfolio: "https://xavierfroment.github.io/cvold",
     titre: "Premier Site CV (2019)",
     desc: "CV réalisé en HTML, CSS et JavaScript dans le cadre de la formation de développeur web."
@@ -10,10 +10,18 @@ const dataportfolio = [
   {
     id: 2,
     preview: "jpg/colorshade.png",
-    altpreview: "mon nuancier",
+    altpreview: "Mon nuancier",
     lienfolio: "https://xavierfroment.github.io/color-shade/",
     titre: "Mon Nuancier",
-    desc: "Projet réalisé en React.JS avec la bibliothèque Values.JS, pour vous aider dans vos choix de couleurs"
+    desc: "Projet réalisé en React.JS avec la bibliothèque Values.JS, pour vous aider dans vos choix de couleurs."
+  },
+  {
+    id: 2,
+    preview: "jpg/adoptapet.png",
+    altpreview: "Adopt'a pet",
+    lienfolio: "https://xavierfroment.github.io/adopt-a-pet/",
+    titre: "Adopt'A Pet",
+    desc: "Démo de site pour l'adoption d'animaux, réalisé en React.JS avec du Bootstrap 4 pour le visuel."
   }
   /*ADD SOME DATA HERE !! */ 
 
