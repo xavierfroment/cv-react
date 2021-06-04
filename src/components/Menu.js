3e545cc8ce77b5558e2d311c500ed9c6a46f854e
fromentxavier <fromentxavier@gmail.com>
2021-06-04 10:06:23 +0200

    fix collapsed menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Menu.css'
 
 function Menu() {
   return (
     <Fragment>
-      <Navbar bg="dark" variant="dark" expand="lg" sticky="top" >
+      <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg" sticky="top" >
         <Navbar.Brand>Xavier FROMENT</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavLink className="nav-link" to="/" exact>Accueil</NavLink>
-            <NavLink className="nav-link" to="/skills">Compétences</NavLink>
-            <NavLink className="nav-link" to="/career">Parcours</NavLink>
-            <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
-            <NavLink className="nav-link" to="/about">A Propos</NavLink>
+          <Nav className="mr-auto text-center">
+            <Nav.Link eventKey="1" as={Link} className="nav-link" to="/">Accueil</Nav.Link>
+            <Nav.Link eventKey="2" as={Link} className="nav-link" to="/skills">Compétences</Nav.Link>
+            <Nav.Link eventKey="3" as={Link} className="nav-link" to="/career">Parcours</Nav.Link>
+            <Nav.Link eventKey="4" as={Link} className="nav-link" to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link eventKey="5" as={Link} className="nav-link" to="/about">A Propos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
